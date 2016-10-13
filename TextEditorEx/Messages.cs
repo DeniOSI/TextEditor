54325015b01e24d0c899132f4231d2035796453f
DeniOSI <geksabyte@gmail.com>
2016-10-13 23:55:22 +0300

    Add error message

---
+++ TextEditorEx/Messages.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditorEx
+{
+    interface IMessage
+    {
+         void ShowMessage(string message);
+
+         void ShowError(string message);
+
+         void ShowExclamation(string massage);
+      
+    }
+
+    class Messages: IMessage
+    {
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message, "собщение",  MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+        public void ShowError(string message)
+        {
+            MessageBox.Show(message, "Ошибка", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+        public void ShowExclamation(string massage)
+        {
+            MessageBox.Show(massage, "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+    }
+}
